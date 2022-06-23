2ed1bf85d672a4b30b9e52791b9a2a6db8f71300
AL3XBR1TO <103543739+alexbr-alves@users.noreply.github.com>
2022-06-23 14:12:42 -0300

    1º commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import Rotas from "./src/rotas";
+import { TemaProvider } from "./src/contexts/TemaContext";
+import { AutenticacaoProvider } from "./src/contexts/AutenticacaoContext";
+import { ProdutosProvider } from "./src/contexts/ProdutosContext";
+
+export default function App() {
+  return (
+    <TemaProvider>
+      <AutenticacaoProvider>
+        <ProdutosProvider>
+            <Rotas />
+        </ProdutosProvider>
+      </AutenticacaoProvider>
+    </TemaProvider>
+    
+  );
+}
